87af26ddeca6ae6011d20571a489a5da515072eb
Usik <59968956+usikorumv@users.noreply.github.com>
2022-09-11 15:55:31 +0600

    Added Pre Adaptivity

--- lib/features/codingsolution/presentation/pages/auth/register_page.dart
+++ lib/features/codingsolution/presentation/pages/auth/register_page.dart
@@ -60,127 +60,139 @@ class _RegisterPageState extends State<RegisterPage> {
               state.message.toString().toToastError();
             }
           },
-          child: Center(
-            child: SingleChildScrollView(
+          child: SingleChildScrollView(
+            child: Center(
               child: Padding(
                 padding: EdgeInsets.all(Dimens.space24),
                 child: Form(
                   key: _keyForm,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      TextF(
-                        key: const Key("email"),
-                        curFocusNode: _fnEmail,
-                        nextFocusNode: _fnPassword,
-                        textInputAction: TextInputAction.next,
-                        controller: _conEmail,
-                        keyboardType: TextInputType.emailAddress,
-                        prefixIcon: Icon(
-                          Icons.alternate_email,
-                          color: Theme.of(context).textTheme.bodyText1?.color,
-                        ),
-                        hintText: 'johndoe@gmail.com',
-                        hint: "",
-                        validator: (String? value) => value != null
-                            ? (!value.isValidEmail() ? "" : null)
-                            : null,
-                      ),
-                      TextF(
-                        key: const Key("password"),
-                        curFocusNode: _fnPassword,
-                        nextFocusNode: _fnPasswordRepeat,
-                        textInputAction: TextInputAction.next,
-                        controller: _conPassword,
-                        keyboardType: TextInputType.text,
-                        prefixIcon: Icon(
-                          Icons.lock_outline,
-                          color: Theme.of(context).textTheme.bodyText1?.color,
-                        ),
-                        obscureText: _isPasswordHide,
-                        hintText: '••••••••••••',
-                        maxLine: 1,
-                        hint: "",
-                        suffixIcon: IconButton(
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          onPressed: () {
-                            setState(
-                              () {
-                                _isPasswordHide = !_isPasswordHide;
-                              },
-                            );
-                          },
-                          icon: Icon(
-                            _isPasswordHide
-                                ? Icons.visibility_off
-                                : Icons.visibility,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(maxWidth: 600),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "Register your Account",
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 30,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        validator: (String? value) => value != null
-                            ? (value.length < 3 ? "" : null)
-                            : null,
-                      ),
-                      TextF(
-                        key: const Key("repeat_password"),
-                        curFocusNode: _fnPasswordRepeat,
-                        textInputAction: TextInputAction.done,
-                        controller: _conPasswordRepeat,
-                        keyboardType: TextInputType.text,
-                        prefixIcon: Icon(
-                          Icons.lock_outline,
-                          color: Theme.of(context).textTheme.bodyText1?.color,
-                        ),
-                        obscureText: _isPasswordRepeatHide,
-                        hintText: '••••••••••••',
-                        maxLine: 1,
-                        hint: "",
-                        suffixIcon: IconButton(
-                          padding: EdgeInsets.zero,
-                          constraints: const BoxConstraints(),
-                          onPressed: () {
-                            setState(
-                              () {
-                                _isPasswordRepeatHide = !_isPasswordRepeatHide;
-                              },
-                            );
-                          },
-                          icon: Icon(
-                            _isPasswordRepeatHide
-                                ? Icons.visibility_off
-                                : Icons.visibility,
+                        TextF(
+                          key: const Key("email"),
+                          curFocusNode: _fnEmail,
+                          nextFocusNode: _fnPassword,
+                          textInputAction: TextInputAction.next,
+                          controller: _conEmail,
+                          keyboardType: TextInputType.emailAddress,
+                          prefixIcon: Icon(
+                            Icons.alternate_email,
+                            color: Theme.of(context).textTheme.bodyText1?.color,
                           ),
+                          hintText: 'example@mail.com',
+                          hint: "",
+                          validator: (String? value) => value != null
+                              ? (!value.isValidEmail() ? "" : null)
+                              : null,
                         ),
-                        validator: (String? value) => value != null
-                            ? (value != _conPassword.text ? "" : null)
-                            : null,
-                      ),
-                      SpacerV(value: Dimens.space24),
-                      Button(
-                        key: const Key("btn_register"),
-                        color: kPrimaryColor,
-                        title: "Register Account",
-                        onPressed: () {
-                          /// Validate form first
-                          if (_keyForm.currentState?.validate() ?? false) {
-                            context.read<RegisterCubit>().register(
-                                  CodeSolutionRegisterParams(
-                                    email: _conEmail.text,
-                                    password: _conPassword.text,
-                                  ),
-                                );
-                          }
-                        },
-                      ),
-                      Button(
-                        key: const Key("btn_login"),
-                        color: kPrimaryColor,
-                        title: "Go to Login Page",
-                        onPressed: () {
-                          context.goNamed(Routes.login.name);
-                        },
-                      ),
-                    ],
+                        TextF(
+                          key: const Key("password"),
+                          curFocusNode: _fnPassword,
+                          nextFocusNode: _fnPasswordRepeat,
+                          textInputAction: TextInputAction.next,
+                          controller: _conPassword,
+                          keyboardType: TextInputType.text,
+                          prefixIcon: Icon(
+                            Icons.lock_outline,
+                            color: Theme.of(context).textTheme.bodyText1?.color,
+                          ),
+                          obscureText: _isPasswordHide,
+                          hintText: '••••••••••••',
+                          maxLine: 1,
+                          hint: "",
+                          suffixIcon: IconButton(
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            onPressed: () {
+                              setState(
+                                () {
+                                  _isPasswordHide = !_isPasswordHide;
+                                },
+                              );
+                            },
+                            icon: Icon(
+                              _isPasswordHide
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                          ),
+                          validator: (String? value) => value != null
+                              ? (value.length < 3 ? "" : null)
+                              : null,
+                        ),
+                        TextF(
+                          key: const Key("repeat_password"),
+                          curFocusNode: _fnPasswordRepeat,
+                          textInputAction: TextInputAction.done,
+                          controller: _conPasswordRepeat,
+                          keyboardType: TextInputType.text,
+                          prefixIcon: Icon(
+                            Icons.lock_outline,
+                            color: Theme.of(context).textTheme.bodyText1?.color,
+                          ),
+                          obscureText: _isPasswordRepeatHide,
+                          hintText: '••••••••••••',
+                          maxLine: 1,
+                          hint: "",
+                          suffixIcon: IconButton(
+                            padding: EdgeInsets.zero,
+                            constraints: const BoxConstraints(),
+                            onPressed: () {
+                              setState(
+                                () {
+                                  _isPasswordRepeatHide =
+                                      !_isPasswordRepeatHide;
+                                },
+                              );
+                            },
+                            icon: Icon(
+                              _isPasswordRepeatHide
+                                  ? Icons.visibility_off
+                                  : Icons.visibility,
+                            ),
+                          ),
+                          validator: (String? value) => value != null
+                              ? (value != _conPassword.text ? "" : null)
+                              : null,
+                        ),
+                        SpacerV(value: Dimens.space24),
+                        Button(
+                          key: const Key("btn_register"),
+                          color: kPrimaryColor,
+                          title: "Register",
+                          onPressed: () {
+                            /// Validate form first
+                            if (_keyForm.currentState?.validate() ?? false) {
+                              context.read<RegisterCubit>().register(
+                                    CodeSolutionRegisterParams(
+                                      email: _conEmail.text,
+                                      password: _conPassword.text,
+                                    ),
+                                  );
+                            }
+                          },
+                        ),
+                        ButtonText(
+                          title: "Back to Login Page",
+                          onPressed: () {
+                            /// Direct to register page
+                            context.goNamed(Routes.login.name);
+                          },
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
