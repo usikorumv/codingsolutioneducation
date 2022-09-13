a969c1930098ccdba63ac57dcfe5cf0961b47aaf
Usik <59968956+usikorumv@users.noreply.github.com>
2022-09-13 08:46:02 +0600

    reformatted

--- lib/common/styles.dart
+++ lib/common/styles.dart
@@ -1,8 +1,9 @@
+import 'package:codingsolution/common/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-ThemeData themeData(){
+ThemeData themeData() {
   return ThemeData(
       scaffoldBackgroundColor: const Color.fromRGBO(7, 17, 26, 1),
       colorScheme: const ColorScheme.dark(),
@@ -11,25 +12,23 @@ ThemeData themeData(){
       iconTheme: IconThemeData(color: Colors.grey[400]),
       textButtonTheme: TextButtonThemeData(
           style: ButtonStyle(
-            foregroundColor: MaterialStateProperty.all(Colors.white),
-          )
-      )
-  );
+        foregroundColor: MaterialStateProperty.all(Colors.white),
+      )));
 }
-CupertinoThemeData cupthemeData(){
+
+CupertinoThemeData cupthemeData() {
   return const CupertinoThemeData(
-      scaffoldBackgroundColor: Color.fromRGBO(7, 17, 26, 1),
-      primaryColor: CupertinoDynamicColor.withBrightness(
-        color: CupertinoColors.white,
-        darkColor: CupertinoColors.black,
-      ),
-      brightness: Brightness.dark,
-      primaryContrastingColor: Colors.yellow,
-
+    scaffoldBackgroundColor: Color.fromRGBO(7, 17, 26, 1),
+    primaryColor: CupertinoDynamicColor.withBrightness(
+      color: CupertinoColors.white,
+      darkColor: CupertinoColors.black,
+    ),
+    brightness: Brightness.dark,
+    primaryContrastingColor: Colors.yellow,
   );
 }
 
-TextStyle logoTextStyle(double mediaQuerySize,Color color){
+TextStyle logoTextStyle(double mediaQuerySize, Color color) {
   return GoogleFonts.josefinSans(
     fontSize: mediaQuerySize * .025,
     fontWeight: FontWeight.bold,
@@ -38,7 +37,7 @@ TextStyle logoTextStyle(double mediaQuerySize,Color color){
   );
 }
 
-TextStyle menuTextStyle(double mediaQuerySize){
+TextStyle menuTextStyle(double mediaQuerySize) {
   return TextStyle(
     //color:kPrimaryColor,
     fontSize: mediaQuerySize * .01,
